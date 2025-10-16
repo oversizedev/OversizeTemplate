02dd9c345efdecb6673df03899706ec36c576aed
Alexander Romanov <aromanov07@gmail.com>
2025-10-17 00:01:22 +0300

    Ubdate MVVM-Reducer

---
+++ File Templates/MVVM-Reducer.xctemplate/___VARIABLE_productName___/___VARIABLE_productName___.swift
@@ -0,0 +1,10 @@
+// ___FILEHEADER___
+
+import OversizeArchitecture
+
+@Module
+public enum ___FILEBASENAMEASIDENTIFIER___: ModuleProtocol {}
+
+public struct ___VARIABLE_productName___Input: Sendable {}
+
+public struct ___VARIABLE_productName___Output: Sendable {}
